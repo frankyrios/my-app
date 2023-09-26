18568c3bd3739ed8e22c818444efc613f874b6b9
FrankyRios <114372979+frankyrios@users.noreply.github.com>
2023-09-26 09:48:39 -0500

    5 commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [so, setSo] = useState();
-
+  const [url, setUrl] = useState();
 
   useEffect(() => {
     const userAgent = navigator.userAgent
 
     if (/(android|iphone|ipad)/i.test(userAgent)) {
       if (/android/i.test(userAgent)) {
-        setSo('android')
+        setUrl('https://bel3-ent4.osapps.bi.com.gt/fl?fintech_name=fonyou&fintech_id=202104086749&fintech_logo=https://recargo.miclaro.com.gt/img/logo-claro.jpg&requestId=40C12FC8-576E-4F15-BBE8-A6A749248D80&type=webmovil')
       } else if (/iphone|ipad/i.test(userAgent)) {
-        setSo('ios')
+        setUrl('https://belappgt.bi.com.gt/fl?appName=fonyou&appId=202104086749&requestId=40C12FC8-576E-4F15-BBE8-A6A749248D80&logoURL=https://recargo.miclaro.com.gt/img/logo-claro.jpg&type=webmovil')
       }
     } else {
-      setSo('escritorio')
+      setUrl('https://www.corporacionbi.com/gt/bancoindustrial/')
     }
   }, []);
 
@@ -24,11 +23,11 @@ function App() {
       <header className="App-header">
         <a
           className="App-link"
-          href={so === 'android' ? "https://instagram.com/reels/?launch_app_store=1" : (so === 'ios' ? "https://apps.apple.com/app/instagram/id389801252" : "https://instagram.com")}
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Instagram
+          BI
         </a>
 
       </header>
